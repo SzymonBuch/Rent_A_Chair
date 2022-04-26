83d52e9f10e8fc326d28d0b39ccb0440e00ad06b
BZiolkow <bartekzet70@gmail.com>
2022-04-26 21:27:30 +0200

    Added forms by blind mole

--- Rent_A_Chair/Rent_A_Chair/urls.py
+++ Rent_A_Chair/Rent_A_Chair/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from website.views import index, storage, worker, product
 
@@ -25,4 +25,5 @@ urlpatterns = [
     path('storage', storage, name='storage'),
     path('worker', worker, name='worker'),
     path('product', product, name='product'),
+    path('Rent_A_Chair_App/', include('Rent_A_Chair_App.urls'), name='product'),
 ]
